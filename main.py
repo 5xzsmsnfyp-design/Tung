abdcc37a47193ce0468ea7a560feabdf8947e913
5xzsmsnfyp-design <5xzsmsnfyp@privaterelay.appleid.com>
2026-08-19 21:41:43 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+driver.get("https://google.com")
+print("Site Başlığı:", driver.title)
+driver.quit()
